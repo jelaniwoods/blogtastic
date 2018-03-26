ba9d09751abd9fb8677705865e83346e596094a0
Windows Letgoner <jmw686@nyu.edu>
2018-03-26 17:14:25 -0500

    fixed add, made sure all props were NOT NULL

--- add.php
+++ add.php
@@ -10,18 +10,21 @@
 		$details = $_POST['details'];
 		$time = strftime("%X");//time
 		$date = strftime("%B %d, %Y");//date
+		$time_edited = strftime("%X");//time
+		$date_edited = strftime("%B %d, %Y");//date
 		$decision ="no";
-        $con = mysqli_connect("localhost", "root","root", "blogtastic") or die(mysqli_error()); //Connect to server
-        // echo $date." < ";
+        $con = mysqli_connect("localhost", "root", "root", "blogtastic") or die(mysqli_error());
+
+        // echo $details." ".$time." ".$date." ".$decision;
 		 //Connect to database
-		foreach($_POST['public'] as $each_check) //gets the data from the checkbox
- 		{
- 			if($each_check !=null ){ //checks if the checkbox is checked
- 				$decision = "yes"; //sets teh value
+		foreach($_POST['public'] as $each_check) {
+ 			if($each_check !=null ) { //checks if the checkbox is checked
+                 $decision = "yes"; //sets teh value
+                 echo "FINE";
  			}
  		}
 		
-		mysqli_query($con, "INSERT INTO list (details, date_posted, time_posted, public) VALUES ('$details','$date','$time','$decision')"); //SQL query
+		mysqli_query($con, "INSERT INTO list (details, date_posted, time_posted, date_edited, time_edited, public) VALUES ('$details','$date','$time', '$date_edited', '$time_edited', '$decision')"); //SQL query
 		header("location: home.php");
 	}
 	else
