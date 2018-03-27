707b34879dc2a66f819f960527798ef98cf31770
Windows Letgoner <jmw686@nyu.edu>
2018-03-26 19:32:51 -0500

    z

--- delete.php
+++ delete.php
@@ -1,12 +1,11 @@
 <?php
-	session_start(); //starts the session
-	if($_SESSION['user']){ //checks if user is logged in
+	session_start();
+	if($_SESSION['user']) { 
 	}
-	else{
-		header("location:index.php"); // redirects if user is not logged in
+	else {
+		header("location:index.php");
 	}
-	if($_SERVER['REQUEST_METHOD'] == "GET")
-	{
+	if($_SERVER['REQUEST_METHOD'] == "GET") {
 		$con = mysqli_connect("localhost", "root","root", "blogtastic") or die(mysqli_error()); //Connect to server
 		$id = $_GET['id'];
 		mysqli_query($con, "DELETE FROM list WHERE id='$id'");
