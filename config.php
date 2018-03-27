987441b50ced1d8bba60595bdfc7bc27cbf7113a
Windows Letgoner <jmw686@nyu.edu>
2018-03-26 19:42:25 -0500

    yeah ok

---
+++ config.php
@@ -0,0 +1,6 @@
+<?php
+    $db_server = "localhost"; 
+    $db_user = "root";  // Database username
+    $db_password = "root";   // Database password
+    $db_name = "blogtastic";   // Database NAME
+?>
